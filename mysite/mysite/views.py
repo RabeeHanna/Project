25036c3d650cf36e130692d66d1ef35392e02b51
Rabee Hanna <Rabee.Hanna@gmail.com>
2013-05-24 18:35:20 -0400

    fixed some of selection page

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -7,7 +7,7 @@ def home(request):
 	return render(request, 'home.html', {'current_date': now})
 
 def selection(request):
-	return render(request, 'select.html')
+	return render(request, 'selection.html')
 
 def weather(request):
 	return render(request, 'weather.html', {'province':'ON', 'city':'TORONTO'})
